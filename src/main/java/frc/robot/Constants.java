83c870ef90cf264fd0e84ae7361322f3a70f2177
princerodrick59 <116687818+princerodrick59@users.noreply.github.com>
2025-01-28 20:16:07 -0600

    worked on elevator subsystem and added elevator constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,14 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.pathplanner.lib.config.PIDConstants;
 
 import edu.wpi.first.math.Matrix;
@@ -13,7 +21,11 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
 import swervelib.math.Matter;
 
 public final class Constants {
@@ -62,5 +74,69 @@ public final class Constants {
     // Standard Deviations for Pose Estimation
     public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+  }
+
+
+   public static final class ElevatorConstants {
+    public static final int ELEVATOR_LEFT_CAN_ID = 18;
+    public static final int ELEVATOR_RIGHT_CAN_ID = 19;
+
+    // Gear ratio
+    public static final double ELEVATOR_GEAR_RATIO = 5;
+    public static final Distance ELEVATOR_SPROCKET_DIAMETER = Inches.of(1.888);
+
+    public static final Distance FEET_PER_ROTATION = ELEVATOR_SPROCKET_DIAMETER.times(Math.PI).div(ELEVATOR_GEAR_RATIO);//TODO:Fix this to work in feet instad of meters
+
+    // Maximum and minimum extension of the elevator, in meters
+    public static final Distance MAX_HEIGHT = Inches.of(88.5); 
+    public static final Distance MIN_HEIGHT = Inches.of(0); 
+
+    // Current limit of either motor
+    public static final Current CURRENT_LIMIT = Amps.of(0);//TODO:Find Elevator Current Limit from derek
+
+    // Left Motor PID Values
+    public static final double LEFT_KG = 0;
+    public static final double LEFT_KS = 0;
+    public static final double LEFT_KV = 0;
+    public static final double LEFT_KP = 0;
+    public static final double LEFT_KI = 0;
+    public static final double LEFT_KD = 0;
+
+    // Right Motor PID Values
+    public static final double RIGHT_KG = 0;
+    public static final double RIGHT_KS = 0;
+    public static final double RIGHT_KV = 0;
+    public static final double RIGHT_KP = 0;
+    public static final double RIGHT_KI = 0;
+    public static final double RIGHT_KD = 0;
+
+
+    public static final MotorOutputConfigs RIGHT_MOTOR_CONFIGS = new MotorOutputConfigs()
+        .withInverted(InvertedValue.CounterClockwise_Positive)
+        .withNeutralMode(NeutralModeValue.Brake);
+
+    // Elevator Positions
+    public static final Distance STATION_POSITION = Inches.of(0);
+    public static final Distance L1_POSITION = Inches.of(0);
+    public static final Distance L2_POSITION = Inches.of(0);
+    public static final Distance L3_POSITION = Inches.of(0);
+    public static final Distance L4_POSITION = Inches.of(0);
+    public static final Distance ALGAE_POSITION = Inches.of(0);
+
+    // Maximum velocity of the Motors, in Rotations per Second
+    public static final double MAX_VELOCITY_RPS = 0; //TODO:Change this
+
+    // Maximum acceleration of the Motors, in Rotations per Second
+    public static final double MAX_ACCEL_RPS = 0; //TODO:Change this
+
+    public static final Voltage MAX_VOLTS = Volts.of(8);//TODO:Check with Derek about Max Volts
+
+    // Creates new set states for the Trapezoid Profile
+    public static final TrapezoidProfile.State STOW_GOAL = new TrapezoidProfile.State(MIN_HEIGHT.magnitude(), 0);
+    public static final TrapezoidProfile.State STATION_GOAL = new TrapezoidProfile.State(STATION_POSITION.magnitude(), 0);
+    public static final TrapezoidProfile.State L1_GOAL = new TrapezoidProfile.State(L1_POSITION.magnitude(), 0);
+    public static final TrapezoidProfile.State L2_GOAL = new TrapezoidProfile.State(L2_POSITION.magnitude(), 0);
+    public static final TrapezoidProfile.State L3_GOAL = new TrapezoidProfile.State(L3_POSITION.magnitude(), 0);
+    public static final TrapezoidProfile.State L4_GOAL = new TrapezoidProfile.State(L4_POSITION.magnitude(), 0);
   }
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,16 +4,43 @@
 
 package frc.robot.subsystems;
 
-import javax.swing.text.Position;
+import static edu.wpi.first.units.Units.*;
 
+import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.Constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
   // Creating the Motor Controller objects
@@ -31,10 +58,180 @@ public class ElevatorSubsystem extends SubsystemBase {
   private PositionVoltage elevatorRequest;
   private VoltageOut voltageRequest;
 
-  public ElevatorSubsystem() {}
+  private final Mechanism2d elevator2d;
+  private final MechanismRoot2d elevatorRoot;
+  private final MechanismLigament2d elevatorLigament;
+
+  private final SendableChooser<TrapezoidProfile.State> elevatorChooser;
+
+  private final SysIdRoutine sysIdRoutine;
+
+  public ElevatorSubsystem() {
+    
+    elevatorLeftMotor = new TalonFX(ElevatorConstants.ELEVATOR_LEFT_CAN_ID);
+    elevatorRightMotor = new TalonFX(ElevatorConstants.ELEVATOR_RIGHT_CAN_ID);
+
+    elevatorFollower = new Follower(elevatorLeftMotor.getDeviceID(), false);
+    elevatorRightMotor.setControl(elevatorFollower);
+
+    elevatorLeftConfig = new TalonFXConfiguration()
+      .withSlot0(new Slot0Configs()
+                  .withKG(ElevatorConstants.LEFT_KG)
+                  .withKS(ElevatorConstants.LEFT_KS)
+                  .withKV(ElevatorConstants.LEFT_KV)
+                  .withKI(ElevatorConstants.LEFT_KP)
+                  .withKD(ElevatorConstants.LEFT_KI)
+                  .withKD(ElevatorConstants.LEFT_KD))
+
+                  .withCurrentLimits( new CurrentLimitsConfigs()
+                                    .withStatorCurrentLimit(ElevatorConstants.CURRENT_LIMIT)
+                                    .withSupplyCurrentLimit(ElevatorConstants.CURRENT_LIMIT)
+                                    .withStatorCurrentLimitEnable(true)
+                                    .withSupplyCurrentLimitEnable(true))
+
+                  .withSoftwareLimitSwitch(new SoftwareLimitSwitchConfigs()
+                                              .withReverseSoftLimitThreshold(ElevatorConstants.MIN_HEIGHT.magnitude() / ElevatorConstants.FEET_PER_ROTATION.magnitude())
+                                              .withReverseSoftLimitEnable(true)
+                                              .withForwardSoftLimitThreshold(ElevatorConstants.MAX_HEIGHT.magnitude() / ElevatorConstants.FEET_PER_ROTATION.magnitude())
+                                              .withForwardSoftLimitEnable(true))
+
+                  .withMotorOutput(new MotorOutputConfigs()
+                                  .withInverted(InvertedValue.CounterClockwise_Positive)
+                                  .withNeutralMode(NeutralModeValue.Brake));
+
+      elevatorLeftConfig.Voltage.withPeakForwardVoltage(ElevatorConstants.MAX_VOLTS.magnitude())
+      .withPeakReverseVoltage(ElevatorConstants.MAX_VOLTS.magnitude());
+
+      elevatorRightConfig = new TalonFXConfiguration()
+      .withSlot0(new Slot0Configs()
+                  .withKG(ElevatorConstants.RIGHT_KG)
+                  .withKS(ElevatorConstants.RIGHT_KS)
+                  .withKV(ElevatorConstants.RIGHT_KV)
+                  .withKI(ElevatorConstants.RIGHT_KP)
+                  .withKD(ElevatorConstants.RIGHT_KI)
+                  .withKD(ElevatorConstants.RIGHT_KD))
+
+                  .withCurrentLimits( new CurrentLimitsConfigs()
+                                    .withStatorCurrentLimit(ElevatorConstants.CURRENT_LIMIT)
+                                    .withSupplyCurrentLimit(ElevatorConstants.CURRENT_LIMIT)
+                                    .withStatorCurrentLimitEnable(true)
+                                    .withSupplyCurrentLimitEnable(true))
+
+                  .withSoftwareLimitSwitch(new SoftwareLimitSwitchConfigs()
+                                              .withReverseSoftLimitThreshold(ElevatorConstants.MIN_HEIGHT.magnitude() / ElevatorConstants.FEET_PER_ROTATION.magnitude())
+                                              .withReverseSoftLimitEnable(true)
+                                              .withForwardSoftLimitThreshold(ElevatorConstants.MAX_HEIGHT.magnitude() / ElevatorConstants.FEET_PER_ROTATION.magnitude())
+                                              .withForwardSoftLimitEnable(true))
+
+                  .withMotorOutput(new MotorOutputConfigs()
+                                  .withInverted(InvertedValue.CounterClockwise_Positive)
+                                  .withNeutralMode(NeutralModeValue.Brake));
+
+      elevatorRightConfig.Voltage.withPeakForwardVoltage(ElevatorConstants.MAX_VOLTS.magnitude())
+        .withPeakReverseVoltage(ElevatorConstants.MAX_VOLTS.magnitude());
+  
+    
+    elevatorRequest = new PositionVoltage(0).withSlot(0); //TODO:Check what this does
+    voltageRequest = new VoltageOut(Volts.of(0)); //TODO:Check what this does
+
+    currentElevatorState = new TrapezoidProfile.State();
+
+    elevatorLeftMotor.getConfigurator().apply(elevatorLeftConfig);
+    elevatorRightMotor.getConfigurator().apply(elevatorRightConfig);
+
+    elevatorLeftMotor.setPosition(Rotations.of(ElevatorConstants.MIN_HEIGHT.magnitude() / ElevatorConstants.FEET_PER_ROTATION.magnitude()));
+
+    elevatorProfile = new TrapezoidProfile(
+      new TrapezoidProfile.Constraints(ElevatorConstants.MAX_VELOCITY_RPS, ElevatorConstants.MAX_ACCEL_RPS));
+
+    sysIdRoutine = new SysIdRoutine(
+      new SysIdRoutine.Config(
+        Volts.of(1).per(Second),
+        Volts.of(4),
+        Seconds.of(1),
+        (state) -> SignalLogger.writeString("SysIdTranslation_State", state.toString())), 
+      new SysIdRoutine.Mechanism(
+        output -> elevatorLeftMotor.setControl(voltageRequest.withOutput(output)),
+        null,
+        this));
+
+
+  elevator2d = new Mechanism2d(1, 1);
+  elevatorRoot = elevator2d.getRoot("Base", 0.5, 0.5);
+
+  elevatorLigament = elevatorRoot.append(new MechanismLigament2d("ElevatorExt", 1, 90));
+
+  elevatorChooser = new SendableChooser<TrapezoidProfile.State>();
+  elevatorChooser.addOption("-----L4-----", ElevatorConstants.L4_GOAL);
+  elevatorChooser.addOption("-----L3-----", ElevatorConstants.L3_GOAL);
+  elevatorChooser.addOption("-----L2-----", ElevatorConstants.L2_GOAL);
+  elevatorChooser.addOption("-----L1-----", ElevatorConstants.L1_GOAL);
+  elevatorChooser.setDefaultOption("Stow", ElevatorConstants.STOW_GOAL);
+  elevatorChooser.addOption("Coral Station", ElevatorConstants.STATION_GOAL);
+
+
+  SmartDashboard.putData("Elevator", this);
+  SmartDashboard.putData("Elevator/Elevator2d", elevator2d);
+  SmartDashboard.putData("Elevator/ElevatorChooser", elevatorChooser);
+  SmartDashboard.putNumber("Elevator/ElevatorSetPoint", currentElevatorState.position);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  private Angle getElevatorRotations() {
+    return elevatorLeftMotor.getPosition().refresh().getValue();
+  }
+
+  private Distance getElevatorHeight() {
+    return ElevatorConstants.FEET_PER_ROTATION.times(getElevatorRotations().in(Rotations));
+  }
+
+  private LinearVelocity getElevatorVelocity() {
+    return FeetPerSecond.of(elevatorLeftMotor.getVelocity().refresh().getValueAsDouble() * ElevatorConstants.FEET_PER_ROTATION.magnitude());
+  }
+
+  private TrapezoidProfile.State getCurrentState() {
+    return new TrapezoidProfile.State(getElevatorHeight().magnitude(), getElevatorVelocity().magnitude());
+  }
+
+  public TrapezoidProfile.State getSelectedState() {
+    return elevatorChooser.getSelected();
+  }
+
+  public void stopElevator() {
+    elevatorLeftMotor.stopMotor();
+  }
+
+  public void applyElevatorProfile(TrapezoidProfile.State goal) {
+    currentElevatorState = elevatorProfile.calculate(.20, currentElevatorState, goal);
+    elevatorRequest.Position = currentElevatorState.position;
+    elevatorRequest.Velocity = currentElevatorState.velocity;
+
+    elevatorLeftMotor.setControl(elevatorRequest);
+  }
+
+  public void setElevatorPosition(double rotations) {
+    elevatorLeftMotor.setControl(elevatorRequest.withPosition(rotations));
+  }
+
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysIdRoutine.quasistatic(direction.kForward);
+  }
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysIdRoutine.dynamic(direction.kForward);
+  }
+
+  public Command setElevatorPositionCommand(TrapezoidProfile.State goalState) {
+    final double deltaTime = 0.02;
+    final Timer timer = new Timer();
+
+    return runOnce(timer::restart)
+    .andThen(run(() -> {
+      currentElevatorState = elevatorProfile.calculate(deltaTime, getCurrentState(), goalState);
+      elevatorLeftMotor.setControl(elevatorRequest.withPosition(currentElevatorState.position));}))
+      .until(() -> timer.hasElapsed(elevatorProfile.totalTime()));
   }
 }
