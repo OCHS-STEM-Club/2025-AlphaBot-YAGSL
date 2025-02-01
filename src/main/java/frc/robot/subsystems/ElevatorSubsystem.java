8f2c18c89e5f920c83556da950cfb04d8d06c090
Daria <dmcpanda1@gmail.com>
2025-02-01 09:58:40 -0600

    Swap to prince
    Signed-off-by: Daria <dmcpanda1@gmail.com>

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -204,7 +204,7 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
   @AutoLogOutput
   public String getSelectedString() {
-    // Systaeaxdsf
+    // Systaeaxdsfa
     return elevatorChooser.getSelected().toString();
   }
 
